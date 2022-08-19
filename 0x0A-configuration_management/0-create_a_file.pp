defdcf10cf3d37e66d0006e1a82d0d6147a4af27
AFOP <4592@holbertonschool.com>
2022-08-19 18:44:15 -0500

    Task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,12 @@
+#creates a file using puppet manifest
+
+$file = '/tmp/school'
+
+file { $file:
+  ensure  => 'file',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
+}
+
