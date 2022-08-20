d48096f6a97c93680703dde58d190adc2a58d8f9
AFOP <4592@holbertonschool.com>
2022-08-19 20:09:39 -0500

    Update Task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #Using Puppet, create a manifest that kills a process named killmenow.
 
-exec { 'kills a process named killmenow':
+exec { 'kills a process killmenow':
    command => 'pkill killmenow',
    provider => shell,
 }
