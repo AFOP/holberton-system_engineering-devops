df98243327e02d328af78a92572beda3e205d829
AFOP <4592@holbertonschool.com>
2022-09-09 11:38:37 -0500

    Task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Using what you did in the task #0,
+extend your Python script to export data in the CSV format.
+"""
+
+import json
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    obj = {}
+    data = ""
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+    name = user.json().get('name')
+
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos/")
+
+    filename = userId + '.csv'
+    with open(filename, mode='w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            lineterminator='\n')
+        for task in todos.json():
+                if task.get('userId') == int(userId):
+                    writer.writerow([userId, name, str(task.get('completed')), task.get('title')])
